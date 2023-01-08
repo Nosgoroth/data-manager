5263261e266511a5f14cf7af51054fb01be2fde4
Nosgoroth <nosgoroth@gmail.com>
2023-01-08 10:13:38 +0100

    I have absolutely no idea

--- do/FoodIntakeDO.js
+++ do/FoodIntakeDO.js
@@ -1,11 +1,19 @@
 (function(){
 
+	// Example of a custom type. Remove the 'return' to enable.
+	// Copy this file to create a new type. No need to declare it anywhere else for it to work
+	// Check PreorderDO.js for a more complex type.
+
 	return;
 
 	customMultiDataObjectEditor.registerDataObject(
 		DataObjects.createDataObjectType({
-			name: "FoodIntake",
-			types: {
+			name: "FoodIntake", // Name in the type dropdown and data file name. No spaces or special characters.
+
+			// Columns in the database. Most common are int/float/string/bool
+			// There's also enum for multiple choice, in the form ["enum", ["Foo", "Bar", "Baz"]]
+			// Check /res/doop/do.js for docs about types
+			types: { 
 				id: "int",
 				time: "string",
 				food: "string",
@@ -14,18 +22,20 @@
 			},
 		}),
 		{
-			_COLsort: "time",
+			_COLsort: "time", // Default sort by what column
 			_reverseSort: true,
 
+			// Table rendering options
 			_renderOptions: {
 				typeoptions: {
 					calories: {
 						label: "kcal"
 					}
 				},
-				hideFields: ["id"]
+				hideFields: ["id"] // Array of columns to hide from table
 			},
 
+			// Form rendering options
 			_formOptions: {
 				typeoptions: {
 					time: {
@@ -40,7 +50,7 @@
 						label: "Calories (kcal)"
 					}
 				},
-				hideFields: ["id"]
+				hideFields: ["id"] // Array of columns to hide from form
 			}
 		}
 	);
